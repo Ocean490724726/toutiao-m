7cab74de4806891c2dfd71dfa89ed161a0b36414
zhangheng <490724276@qq.com>
2022-10-16 20:17:07 +0800

    引入字体图标和vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 // 引入全局样式文件
 import '@/styles/index.less'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
